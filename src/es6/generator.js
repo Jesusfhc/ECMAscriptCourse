82334404dfe3d285dfa6d5c5b829ba64d259df53
Jesus Hernandez <jjesusfhernandezc@gmail.com>
2023-07-16 19:23:20 -0400

    terminada la parte del ECMA script 6

---
+++ src/es6/generator.js
@@ -0,0 +1,16 @@
+// generator
+
+function* iterate(array) { //reserve word function* al hacer esto iniciamos un generador
+    for (let value of array) {
+        yield value
+    }
+}
+
+const it = iterate(['oscar', 'david', 'ulices', 'jennifer', 'ana']);
+
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+
+
+//cada console log accede a un elemento distinto del array
---
+++ src/es6/set-add.js
@@ -0,0 +1,8 @@
+// Es algo parecido a un array, solo que este no permite elementos repetidos, solo unicos
+
+const list = new Set();
+
+list.add('item1');
+list.add('item2').add('item3')
+
+console.log(list);
